b58b0407cff4ebbcbe18ba4a490695c485d2a34b
maniglia <a.klede@hotmail.it>
2020-12-21 16:45:55 +0100

    Log fix

--- Src/01/Sink/KSociety.Log.Serilog.Sinks.RabbitMq/Sinks/RabbitMq/Output/LogEventFormatter.cs
+++ Src/01/Sink/KSociety.Log.Serilog.Sinks.RabbitMq/Sinks/RabbitMq/Output/LogEventFormatter.cs
@@ -11,7 +11,7 @@ namespace KSociety.Log.Serilog.Sinks.RabbitMq.Sinks.RabbitMq.Output
         public void Format(LogEvent logEvent, TextWriter output)
         {
             //output.Write("Timestamp - {0} | Level - {1} | Message {2} {3}", logEvent.Timestamp, logEvent.Level, logEvent.MessageTemplate, output.NewLine);
-            output.Write("Message {0} {1}", logEvent.MessageTemplate, output.NewLine);
+            output.Write("{0} {1}", logEvent.MessageTemplate, output.NewLine);
             if (logEvent.Exception != null)
             {
                 output.Write("Exception - {0}", logEvent.Exception);
